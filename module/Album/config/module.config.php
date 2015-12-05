69350006c670fe6061980bfd27400fb184b3303e
anubhawbt <anubhawbt@localhost>
2015-12-05 17:12:29 +0530

    upload intial code

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,44 @@
+<?php
+
+return array(
+     'controllers' => array(
+         'invokables' => array(
+             'Album\Controller\Album' => 'Album\Controller\AlbumController',
+         ),
+     ),
+    
+    'router' => array(
+         'routes' => array(
+             'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+             'album' => array(
+                 'type'    => 'segment',
+                 'options' => array(
+                     'route'    => '/album[/:action][/:id]',
+                     'constraints' => array(
+                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                         'id'     => '[0-9]+',
+                     ),
+                     'defaults' => array(
+                         'controller' => 'Album\Controller\Album',
+                         'action'     => 'index',
+                     ),
+                 ),
+             ),
+         ),
+     ),
+     'view_manager' => array(
+         'template_path_stack' => array(
+             'album' => __DIR__ . '/../view',
+         ),
+     ),
+ );
+
