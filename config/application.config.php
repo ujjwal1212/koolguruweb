846f8400405eda77a387f58e2d55de9e47af7bc1
dishuTRI <dishuTRI@localhost>
2015-12-15 18:11:24 +0530

    login module page

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Application',
         'ZF2AuthAcl',
+        'admin',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
