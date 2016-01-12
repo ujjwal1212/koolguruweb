b6a0275ec0ddc23a992f8479e52ead6b8ee503f7
dishuTRI <dishuTRI@localhost>
2016-01-12 08:19:19 +0530

    student register page

--- module/Admin/src/Admin/Controller/AdminController.php
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -11,12 +12,157 @@ namespace Admin\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Session\Container;
+use Questionarie\Form\SearchForm;
+use Zend\Db\Sql\Select;
 
-class AdminController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        
+
+class AdminController extends AbstractActionController {
+
+    protected $adapter;
+    protected $StudentMobileTable;
+
+    public function getAdapter() {
+        if (!$this->adapter) {
+            $sm = $this->getServiceLocator();
+            $this->adapter = $sm->get('Zend\Db\Adapter\Adapter');
+        }
+        return $this->adapter;
     }
     
+    public function getStudentMobileTable() {
+        if (!$this->StudentMobileTable) {
+            $sm = '';
+            $sm = $this->getServiceLocator();
+            $this->StudentMobileTable = $sm->get('Student\Model\StudentMobileTable');
+        }
+        return $this->StudentMobileTable;
+    }
+
+    public function indexAction() {
+        
+    }
+
+    public function contactQueryAction() {
+        if (isset($_REQUEST['page'])) {
+            $page = $_REQUEST['page'];
+        } else {
+            $page = 1;
+        }
+        $form = new SearchForm('form_search');
+        $request = $this->getRequest();
+
+        if ($request->isGet()) {
+            $data = $request->getQuery();
+            $form->getInputFilter()->get('list_count')->setRequired(false);
+            $form->getInputFilter()->get('list_count')->setAllowEmpty(true);
+            $list_count = isset($data['list_count']) && trim($data['list_count']) != '' ? $data['list_count'] : 10;
+            $order_by = isset($data['order_by']) && trim($data['order_by']) != '' ? $data['order_by'] : 'id';
+            $order = isset($data['order']) && trim($data['order']) != '' ? $data['order'] : Select::ORDER_DESCENDING;
+            $searchText = isset($data['search_box_value']) ? trim($data['search_box_value']) : Null;
+            $form->get('list_count')->setValue($list_count);
+            $form->get('order_by')->setValue($order_by);
+            $form->get('order')->setValue($order);
+            $form->setData($data);
+            if ($form->isValid()) {
+                $paginator = $this->getStudentMobileTable()->fetchAll(true, $order_by, $order, $searchText);
+            }
+        } else {
+            // grab the paginator from the CenterTable
+            $paginator = $this->getStudentMobileTable()->fetchAll(true, $order_by, $order, $searchText);
+        }
+        $row_count = $paginator->getTotalItemCount();
+        // set the current page to what has been passed in query string, or to 1 if none set
+        $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', $page));
+        // set the number of items per page to 10
+        $paginator->setItemCountPerPage($list_count);
+        $paginator->setPageRange(5);
+        if (isset($data['page'])) {
+            unset($data['page']);
+        }
+        if ($request->isXmlHttpRequest()) {
+            $this->layout('layout/ajax');
+        }
+        $errorMsg = $this->flashMessenger()->getCurrentMessagesFromNamespace('error');
+        $successMsg = $this->flashMessenger()->getCurrentMessagesFromNamespace('success');
+        if (isset($errorMsg) && is_array($errorMsg) && !empty($errorMsg)) {
+            $errorMsg = $errorMsg[0];
+        }
+
+        return new ViewModel(array(
+            'paginator' => $paginator,
+            'row_count' => $row_count,
+            'list_count' => $list_count,
+            'page' => $page,
+            'order_by' => $order_by,
+            'order' => $order,
+            'data' => $data,
+            'form' => $form,
+            'isAjax' => $request->isXmlHttpRequest(),
+            'errorMsg' => $errorMsg,
+            'successMsg' => $successMsg
+        ));
+    }
+
+    public function studentRegisterAction() {
+        if (isset($_REQUEST['page'])) {
+            $page = $_REQUEST['page'];
+        } else {
+            $page = 1;
+        }
+        $form = new SearchForm('form_search');
+        $request = $this->getRequest();
+
+        if ($request->isGet()) {
+            $data = $request->getQuery();
+            $form->getInputFilter()->get('list_count')->setRequired(false);
+            $form->getInputFilter()->get('list_count')->setAllowEmpty(true);
+            $list_count = isset($data['list_count']) && trim($data['list_count']) != '' ? $data['list_count'] : 10;
+            $order_by = isset($data['order_by']) && trim($data['order_by']) != '' ? $data['order_by'] : 'id';
+            $order = isset($data['order']) && trim($data['order']) != '' ? $data['order'] : Select::ORDER_DESCENDING;
+            $searchText = isset($data['search_box_value']) ? trim($data['search_box_value']) : Null;
+            $form->get('list_count')->setValue($list_count);
+            $form->get('order_by')->setValue($order_by);
+            $form->get('order')->setValue($order);
+            $form->setData($data);
+            if ($form->isValid()) {
+                $paginator = $this->getStudentMobileTable()->fetchAll(true, $order_by, $order, $searchText);
+            }
+        } else {
+            // grab the paginator from the CenterTable
+            $paginator = $this->getStudentMobileTable()->fetchAll(true, $order_by, $order, $searchText);
+        }
+        $row_count = $paginator->getTotalItemCount();
+        // set the current page to what has been passed in query string, or to 1 if none set
+        $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', $page));
+        // set the number of items per page to 10
+        $paginator->setItemCountPerPage($list_count);
+        $paginator->setPageRange(5);
+        if (isset($data['page'])) {
+            unset($data['page']);
+        }
+        if ($request->isXmlHttpRequest()) {
+            $this->layout('layout/ajax');
+        }
+        $errorMsg = $this->flashMessenger()->getCurrentMessagesFromNamespace('error');
+        $successMsg = $this->flashMessenger()->getCurrentMessagesFromNamespace('success');
+        if (isset($errorMsg) && is_array($errorMsg) && !empty($errorMsg)) {
+            $errorMsg = $errorMsg[0];
+        }
+
+        return new ViewModel(array(
+            'paginator' => $paginator,
+            'row_count' => $row_count,
+            'list_count' => $list_count,
+            'page' => $page,
+            'order_by' => $order_by,
+            'order' => $order,
+            'data' => $data,
+            'form' => $form,
+            'isAjax' => $request->isXmlHttpRequest(),
+            'errorMsg' => $errorMsg,
+            'successMsg' => $successMsg
+        ));
+    }
+
 }
--- module/Student/src/Student/Model/StudentMobileTable.php
+++ module/Student/src/Student/Model/StudentMobileTable.php
@@ -3,7 +3,6 @@
 namespace Student\Model;
 
 use Zend\Db\TableGateway\TableGateway;
-
 use Student\Model\StudentMobile;
 use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\Select;
@@ -20,23 +19,44 @@ class StudentMobileTable {
     protected $adapter;
     public $table = 'student_mobile';
 
-    public function __construct(TableGateway $tableGateway) {       
+    public function __construct(TableGateway $tableGateway) {
         $this->tableGateway = $tableGateway;
         $this->resultSetPrototype = new ResultSet(ResultSet::TYPE_ARRAY);
     }
 
-      
-    public function fetchAll() {
+    public function fetchAll($paginated = false, $order_by = 'id', $order = 'ASC', $searchText = NULL) {
+        if ($order_by == 'id' || $order_by == 'name' || $order_by = 'mobile') {
+            $order_by = 'sm.' . $order_by;
+        }
         $sql = new Sql($this->tableGateway->getAdapter());
-        $select = $sql->select()->from(array('sm' => 'student_mobile'), array('id', 'mobile','isregistered','student_id','created'));
+        $select = $sql->select()->from(array('sm' => 'student_mobile'), array('id', 'mobile', 'isregistered', 'student_id', 'created'));
+        $select->order($order_by . ' ' . $order);
+        if (isset($searchText) && trim($searchText) != '') {
+            $select->where->like('q.name', "%" . $searchText . "%")
+            ->or->like('q.id', "%" . $searchText . "%");
+        }
+        if ($paginated) {
+            $resultSetPrototype = new ResultSet();
+            $paginatorAdapter = new DbSelect(
+                    // our configured select object
+                    $select,
+                    // the adapter to run it against
+                    $this->tableGateway->getAdapter(),
+                    // the result set to hydrate
+                    $resultSetPrototype
+            );
+            $paginator = new Paginator($paginatorAdapter);
+            return $paginator;
+        }
+
         $statement = $sql->prepareStatementForSqlObject($select);
         $resultset = $this->resultSetPrototype->initialize($statement->execute())
                 ->toArray();
         return $resultset;
     }
 
-    public function saveStudentMobile($student){        
-        $user_data = array(            
+    public function saveStudentMobile($student) {
+        $user_data = array(
             'mobile' => $student['mobile'],
             'isregistered' => $student['isregistered'],
             'student_id' => $student['student_id'],
@@ -46,27 +66,26 @@ class StudentMobileTable {
         $id = $this->tableGateway->lastInsertValue;
         return $id;
     }
-    
-    public function getIsMobileExist($mobile){
+
+    public function getIsMobileExist($mobile) {
         $flg = false;
         $sql = new Sql($this->tableGateway->getAdapter());
         $select = $sql->select();
         $select->from(array('sm' => 'student_mobile'))
-                ->columns(array('isregistered','mobile','student_id'));               
+                ->columns(array('isregistered', 'mobile', 'student_id'));
         $select->where(array('mobile' => $mobile));
-        
+
         $statement = $sql->prepareStatementForSqlObject($select);
         $resultset = array();
         $resultset = $this->resultSetPrototype->initialize($statement->execute())
                 ->toArray();
-        if(!empty($resultset)){
+        if (!empty($resultset)) {
             $flg = true;
         }
         return $flg;
     }
-    
-    
-    public function updateMobileStatus($mobile,$student){       
+
+    public function updateMobileStatus($mobile, $student) {
         $user_data = array();
         $user_data['isregistered'] = 1;
         $user_data['student_id'] = $student;
