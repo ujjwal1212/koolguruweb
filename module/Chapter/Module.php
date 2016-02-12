fba1b050e571e3338d13de58cd2e25c7e2311e00
anubhaw.srivastava <anubhaw.srivastava@localhost>
2016-02-12 18:05:28 +0530

    changes

--- module/Chapter/Module.php
+++ module/Chapter/Module.php
@@ -1,8 +1,55 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Chapter;
 
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Subject\Model\Category;
+use Subject\Model\CategoryTable;
+use Subject\Model\Subject;
+use Subject\Model\SubjectTable;
+
+class Module {
+
+    public function onBootstrap(MvcEvent $e) {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig() {
+        return array(
+            'factories' => array(
+                'Subject\Model\CategoryTable' => function($sm) {
+                    $tableGateway = $sm->get('CategoryTableGateway');
+                    $table = new CategoryTable($tableGateway);
+                    return $table;
+                },
+                'CategoryTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Category());
+                    return new TableGateway('category', $dbAdapter, null, $resultSetPrototype);
+                },
+                
+            )
+        );
+    }
+
+}
