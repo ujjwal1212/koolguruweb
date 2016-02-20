425b3dde1e2c233ab0ed51162763bdb84d35f71f
dishuTRI <dishuTRI@localhost>
2016-02-20 16:19:20 +0530

    layout commit

---
+++ module/Student/src/Student/Model/StudentStatus.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Student\Model;
+
+// Add these import statements
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+class StudentStatus implements InputFilterAwareInterface {
+
+    public $id;
+    public $student_id;    
+    public $registration_status;
+    public $verbal_reg_status;
+    public $marks_obtain_verbal;
+    public $quant_status;
+    public $marks_obtain_quant;
+    public $quant_reg_created;    
+    public $marks_total_verbal;
+    public $verbal_perc;
+    public $marks_total_quant;
+    public $quant_perc;
+
+    public function exchangeArray($data) {
+        $this->id = (isset($data['id'])) ? $data['id'] : null;
+        $this->student_id = (isset($data['student_id'])) ? $data['student_id'] : null;
+        $this->registration_status = (isset($data['registration_status'])) ? $data['registration_status'] : null;
+        $this->verbal_reg_status = (isset($data['verbal_reg_status'])) ? $data['verbal_reg_status'] : null;
+        $this->marks_obtain_verbal = (isset($data['marks_obtain_verbal'])) ? $data['marks_obtain_verbal'] : null;
+        $this->quant_status = (isset($data['quant_status'])) ? $data['quant_status'] : null;
+        $this->quant_reg_created = (isset($data['quant_reg_created'])) ? $data['quant_reg_created'] : null;        
+        $this->marks_total_verbal = (isset($data['marks_total_verbal'])) ? $data['marks_total_verbal'] : null;
+        $this->verbal_perc = (isset($data['verbal_perc'])) ? $data['verbal_perc'] : null;
+        $this->marks_total_quant = (isset($data['marks_total_quant'])) ? $data['marks_total_quant'] : null;
+        $this->quant_perc = (isset($data['quant_perc'])) ? $data['quant_perc'] : null;
+               
+    }
+
+    // Add content to these methods:
+    public function setInputFilter(InputFilterInterface $inputFilter) {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter() {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory = new InputFactory();
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
+
+    // Add the following method:
+    public function getArrayCopy() {
+        return get_object_vars($this);
+    }
+
+}
