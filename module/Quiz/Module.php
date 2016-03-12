d3ece158afd70436cc2609bd0e9d284016cba6a7
anubhawbt <anubhawbt@localhost>
2016-03-12 11:34:35 +0530

    changes

---
+++ module/Quiz/Module.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Quiz;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Package\Model\Quiz;
+use Package\Model\QuizTable;
+
+class Module {
+
+    public function onBootstrap(MvcEvent $e) {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig() {
+        return array(
+            'factories' => array(
+                'Quiz\Model\QuizTable' => function($sm) {
+                    $tableGateway = $sm->get('QuizTableGateway');
+                    $table = new QuizTable($tableGateway);
+                    return $table;
+                },
+                'QuizTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Quiz());
+                    return new TableGateway('quiz', $dbAdapter, null, $resultSetPrototype);
+                },
+                
+            )
+        );
+    }
+
+}
